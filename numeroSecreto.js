cd1b6d1ecb3eb7ac128428ac36a1de5aeeb02c7c
NandaMelooo <mfmelo.de@gmail.com>
2025-02-12 19:49:40 -0300

    Ajuste imagens e style

--- numeroSecreto.js
+++ numeroSecreto.js
@@ -1,1 +1,8 @@
 alert("Testando");
+let numeroSecreto = 29;
+let chute = prompt("Escolha um número entre 1 e 30:")
+
+if (chute == numeroSecreto){
+    console.log ("Você descobriu o número secreto");
+    return true
+}
