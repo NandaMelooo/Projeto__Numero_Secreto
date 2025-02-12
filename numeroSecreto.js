0ed4f1e237843362fba1879a722d6953b18d1744
NandaMelooo <mfmelo.de@gmail.com>
2025-02-12 20:00:23 -0300

    Ajuste código Js

--- numeroSecreto.js
+++ numeroSecreto.js
@@ -1,8 +1,11 @@
 alert("Testando");
 let numeroSecreto = 29;
+console.log(numeroSecreto)
 let chute = prompt("Escolha um número entre 1 e 30:")
 
 if (chute == numeroSecreto){
-    console.log ("Você descobriu o número secreto");
+    alert(`Você descobriu o número secreto ${numeroSecreto}`);
     return true
+} else{
+    alert("Você errou.")
 }
