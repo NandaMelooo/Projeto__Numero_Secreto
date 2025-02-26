ee2e437baf3dc4cf25160cb169868813634d89fc
NandaMelooo <mfmelo.de@gmail.com>
2025-02-26 16:59:31 -0300

    Adicionando  a contagem de tentativas

--- numeroSecreto.js
+++ numeroSecreto.js
@@ -1,11 +1,24 @@
 alert("Testando");
 let numeroSecreto = 29;
-console.log(numeroSecreto)
-let chute = prompt("Escolha um número entre 1 e 30:")
+console.log(numeroSecreto);
+let chute ;
+let tentativas = 1;
 
-if (chute == numeroSecreto){
-    alert(`Você descobriu o número secreto ${numeroSecreto}`);
-    return true
-} else{
-    alert("Você errou.")
+//Enquanto (loop) chute não for igual ao numero secreto
+while(chute != numeroSecreto){
+    chute = prompt("Escolha um número entre 1 e 30:")
+    if (chute == numeroSecreto){
+        alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.` );
+    
+    } else{
+        if(chute>numeroSecreto){
+        alert(`O número Secreto é maior que ${chute}`);
+        }else{
+        alert(`O número Secreto é mamenor que ${chute}`);
+    }
+    //tentativas=tentativas + 1
+    tentativas++;
 }
+}
+
+
